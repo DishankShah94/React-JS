f067c63b95d49030c61ea18cf55a7e822db0b69a
DishankShah94 <dishankshah94@gmail.com>
2023-12-20 23:33:34 +0530

    React Project
    React Project

--- React Project Template/src/Pages/Admin/AllUsers.jsx
+++ React Project Template/src/Pages/Admin/AllUsers.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
-
+import axios, { all } from 'axios';
+import React, { useEffect, useState } from 'react';
 const AllUsers = () => {
-    return (
-        <>
-          allusers  
-        </>
-    );
+  let [allUsers, setAllUsers] = useState([]);
+  let [loader, setLoader] = useState(false);
+  let fetchData = async () => {
+    axios.get('http://localhost:5000/users')
+      .then(function (response) {
+        console.log(response.data);
+        let userdatastr = response.data.map((data) => {
+          console.log(data);
+          return <tr key={data.id}><td>{data.uname}</td></tr>
+        })
+        console.log(userdatastr);
+        setAllUsers(userdatastr);
+        setLoader(true);
+      })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <>
+      <div className="row box">
+        <div className="col">
+          {loader ? <table>
+            <thead>
+              <tr>
+                <th>User Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allUsers}
+
+            </tbody>
+          </table> : <table>
+            <thead>
+              <tr>
+                <th>User Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              No Users Found
+            </tbody>
+          </table>}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AllUsers;
